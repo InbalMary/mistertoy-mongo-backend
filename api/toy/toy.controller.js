e80e27a0030db631ff5c4dcb18dbddbc91f73f7e
InbalMary <inbalby10@gmail.com>
2025-09-08 13:15:51 +0300

    init repo- connect mongodb server

---
+++ api/toy/toy.controller.js
@@ -0,0 +1,119 @@
+import { toyService } from './toy.service.js'
+import { logger } from '../../services/logger.service.js'
+
+export async function getToys(req, res) {
+    try {
+        const filterBy = {
+            txt: req.query.txt || '',
+            price: req.query.price,
+            inStock: req.query.inStock,
+            labels: req.query.labels ? req.query.labels.split(',') : [],
+            sort: req.query.sort,
+            pageIdx: +req.query.pageIdx || 0,
+        }
+        const toys = await toyService.query(filterBy)
+        res.json(toys)
+    } catch (err) {
+        logger.error('Failed to get toys', err)
+        res.status(500).send({ err: 'Failed to get toys' })
+    }
+}
+
+export async function getToyById(req, res) {
+    try {
+        const toyId = req.params.id
+        const toy = await toyService.getById(toyId)
+        res.json(toy)
+    } catch (err) {
+        logger.error('Failed to get toy', err)
+        res.status(500).send({ err: 'Failed to get toy' })
+    }
+}
+
+export async function addToy(req, res) {
+    const { loggedinUser } = req
+
+    try {
+        const toy = req.body
+        toy.owner = loggedinUser
+        const addedToy = await toyService.add(toy)
+        res.json(addedToy)
+    } catch (err) {
+        logger.error('Failed to add toy', err)
+        res.status(500).send({ err: 'Failed to add toy' })
+    }
+}
+
+export async function updateToy(req, res) {
+    try {
+        const toy = { ...req.body, _id: req.params.id }
+        const updatedToy = await toyService.update(toy)
+        res.json(updatedToy)
+    } catch (err) {
+        logger.error('Failed to update toy', err)
+        res.status(500).send({ err: 'Failed to update toy' })
+    }
+}
+
+export async function removeToy(req, res) {
+    try {
+        const toyId = req.params.id
+        const deletedCount = await toyService.remove(toyId)
+        res.send(`${deletedCount} toys removed`)
+    } catch (err) {
+        logger.error('Failed to remove toy', err)
+        res.status(500).send({ err: 'Failed to remove toy' })
+    }
+}
+
+export async function addToyMsg(req, res) {
+    const { loggedinUser } = req
+    try {
+        const toyId = req.params.id
+        const msg = {
+            txt: req.body.txt,
+            by: loggedinUser,
+            createdAt: Date.now(),
+        }
+        const savedMsg = await toyService.addToyMsg(toyId, msg)
+        res.json(savedMsg)
+    } catch (err) {
+        logger.error('Failed to update toy', err)
+        res.status(500).send({ err: 'Failed to update toy' })
+    }
+}
+
+export async function removeToyMsg(req, res) {
+    // const { loggedinUser } = req
+    try {
+        // const toyId = req.params.id
+        // const { msgId } = req.params
+        const { id: toyId, msgId } = req.params
+
+        const removedId = await toyService.removeToyMsg(toyId, msgId)
+        res.send(removedId)
+    } catch (err) {
+        logger.error('Failed to remove toy msg', err)
+        res.status(500).send({ err: 'Failed to remove toy msg' })
+    }
+}
+
+export async function getLabels(req, res) {
+    try {
+        const labels = await toyService.getLabels()
+        res.json(labels)
+    } catch (err) {
+        logger.error('Failed to get labels', err)
+        res.status(500).send({ err: 'Failed to get labels' })
+    }
+}
+
+export async function getLabelStats(req, res) {
+    try {
+        const stats = await toyService.getLabelStats()
+        res.json(stats)
+    } catch (err) {
+        logger.error('Failed to get label stats', err)
+        res.status(500).send({ err: 'Failed to get label stats' })
+    }
+}
---
+++ api/toy/toy.service.js
@@ -0,0 +1,226 @@
+import { ObjectId } from 'mongodb'
+
+import { dbService } from '../../services/db.service.js'
+import { logger } from '../../services/logger.service.js'
+import { utilService } from '../../services/util.service.js'
+
+export const toyService = {
+	remove,
+	query,
+	getById,
+	add,
+	update,
+	getLabels,
+	getLabelStats,
+	addToyMsg,
+	removeToyMsg,
+}
+
+const PAGE_SIZE = 10
+
+async function query(filterBy = { txt: '' }) {
+	try {
+		const criteria = {}
+		if (filterBy.txt) {
+			criteria.name = { $regex: filterBy.txt, $options: 'i' }
+		}
+		if (filterBy.price && !isNaN(parseFloat(filterBy.price))) {
+			criteria.price = { $lte: +filterBy.price }
+		}
+		if (filterBy.inStock === 'true') {
+			criteria.inStock = true
+		} else if (filterBy.inStock === 'false') {
+			criteria.inStock = false
+		}
+		if (filterBy.labels && filterBy.labels.length > 0) {
+			criteria.labels = { $all: filterBy.labels }
+		}
+		console.log('FINAL CRITERIA:', criteria)
+		const collection = await dbService.getCollection('toy')
+		console.log('COLLECTION COUNT:', await collection.countDocuments())
+		console.log('CRITERIA COUNT:', await collection.countDocuments(criteria))
+
+		const sortBy = {}
+		if (filterBy.sort === 'name') {
+			sortBy.name = 1
+		} else if (filterBy.sort === 'price') {
+			sortBy.price = 1
+		} else if (filterBy.sort === 'createdAt') {
+			sortBy.createdAt = 1
+		}
+
+		const pageIdx = filterBy.pageIdx || 0
+		const skip = pageIdx * PAGE_SIZE
+
+		const toys = await collection
+			.find(criteria)
+			.sort(sortBy)
+			// .skip(skip)
+			// .limit(PAGE_SIZE)
+			.toArray()
+
+		return toys
+
+	} catch (err) {
+		logger.error('cannot find toys', err)
+		throw err
+	}
+}
+
+async function getById(toyId) {
+	try {
+		const collection = await dbService.getCollection('toy')
+		const toy = await collection.findOne({ _id: ObjectId.createFromHexString(toyId) })
+		toy.createdAt = toy._id.getTimestamp()
+		return toy
+	} catch (err) {
+		logger.error(`while finding toy ${toyId}`, err)
+		throw err
+	}
+}
+
+async function remove(toyId) {
+	try {
+		const collection = await dbService.getCollection('toy')
+		const { deletedCount } = await collection.deleteOne({ _id: ObjectId.createFromHexString(toyId) })
+		return deletedCount
+	} catch (err) {
+		logger.error(`cannot remove toy ${toyId}`, err)
+		throw err
+	}
+}
+
+async function add(toy) {
+	try {
+		const collection = await dbService.getCollection('toy')
+		await collection.insertOne(toy)
+		return toy
+	} catch (err) {
+		logger.error('cannot insert toy', err)
+		throw err
+	}
+}
+
+async function update(toy) {
+	try {
+		const toyToSave = {
+			vendor: toy.vendor,
+			price: toy.price,
+		}
+		const collection = await dbService.getCollection('toy')
+		await collection.updateOne({ _id: ObjectId.createFromHexString(toy._id) }, { $set: toyToSave })
+		return toy
+	} catch (err) {
+		logger.error(`cannot update toy ${toy._id}`, err)
+		throw err
+	}
+}
+
+async function addToyMsg(toyId, msg) {
+	try {
+		msg.id = utilService.makeId()
+
+		const collection = await dbService.getCollection('toy')
+		await collection.updateOne({ _id: ObjectId.createFromHexString(toyId) }, { $push: { msgs: msg } })
+		return msg
+	} catch (err) {
+		logger.error(`cannot add toy msg ${toyId}`, err)
+		throw err
+	}
+}
+
+async function removeToyMsg(toyId, msgId) {
+	try {
+		const collection = await dbService.getCollection('toy')
+		await collection.updateOne({ _id: ObjectId.createFromHexString(toyId) }, { $pull: { msgs: { id: msgId } } })
+		return msgId
+	} catch (err) {
+		logger.error(`cannot add toy msg ${toyId}`, err)
+		throw err
+	}
+}
+
+async function getLabels() {
+	try {
+		const collection = await dbService.getCollection('toy')
+
+		const labels = await collection.aggregate([
+			{ $unwind: '$labels' },
+			{ $group: { _id: '$labels' } },
+			{ $sort: { _id: 1 } }
+		]).toArray()
+
+		return labels.map(labelDoc => labelDoc._id)
+
+	} catch (err) {
+		logger.error('cannot get labels', err)
+		throw err
+	}
+}
+
+async function getLabelStats() {
+	try {
+		const collection = await dbService.getCollection('toy')
+
+		const stats = await collection.aggregate([
+			{ $match: { labels: { $exists: true, $ne: [] } } },
+			{ $unwind: '$labels' },
+			{
+				$group: {
+					_id: '$labels',
+					prices: { $push: '$price' },
+					total: { $sum: 1 },
+					inStock: {
+						$sum: {
+							$cond: [
+								{ $or: [{ $eq: ['$inStock', true] }, { $eq: ['$inStock', 'true'] }] },
+								1,
+								0
+							]
+						}
+					}
+				}
+			},
+			{
+				$project: {
+					avgPrice: {
+						$round: [
+							{ $avg: '$prices' },
+							2
+						]
+					},
+					total: 1,
+					inStock: 1,
+					percent: {
+						$round: [
+							{
+								$multiply: [
+									{ $divide: ['$inStock', '$total'] },
+									100
+								]
+							},
+							2
+						]
+					}
+				}
+			},
+			{ $sort: { _id: 1 } }
+		]).toArray()
+
+		const labelStats = {}
+		stats.forEach(stat => {
+			labelStats[stat._id] = {
+				avgPrice: stat.avgPrice,
+				total: stat.total,
+				inStock: stat.inStock,
+				percent: stat.percent
+			}
+		})
+
+		return labelStats
+
+	} catch (err) {
+		logger.error('cannot get label stats', err)
+		throw err
+	}
+}
